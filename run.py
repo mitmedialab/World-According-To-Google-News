30476cc6876694ba38419523517b1678ce782044
Kevin Hu <kzh@mit.edu>
2012-10-29 19:49:33 -0400

    Cloned d3 azimuthal example, added minimal Flask framework

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=4000)
